eab1053db76c88d735b0417bdc175866d6b815fe
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2025-04-15 10:51:31 +0200

    Add spring boot to project.

---
+++ src/main/java/pl/thinkdata/droptop/DroptopApplication.java
@@ -0,0 +1,13 @@
+package pl.thinkdata.droptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroptopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/thinkdata/droptop/DroptopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.thinkdata.droptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
