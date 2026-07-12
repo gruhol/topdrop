ffe1d66dc1bd0da2319881c68e1d63902e5e7a19
gruhol <dabrowskiw@gmail.com>
2026-07-12 14:33:48 +0200

    Show raw response.

--- src/main/java/pl/thinkdata/droptop/baselinker/service/BaselinkerSendable.java
+++ src/main/java/pl/thinkdata/droptop/baselinker/service/BaselinkerSendable.java
@@ -2,16 +2,21 @@ package pl.thinkdata.droptop.baselinker.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 
 public interface BaselinkerSendable<RES, REQ> {
+    Logger log = LoggerFactory.getLogger(BaselinkerSendable.class);
+
     RES sendRequest(REQ req) throws JsonProcessingException;
 
     default RES mapToResponse(ResponseEntity<String> response, Class<RES> clazz) {
         try {
             return new ObjectMapper().readValue(response.getBody(), clazz);
         } catch (Exception e) {
-            throw new RuntimeException("Deserialization Error: " + clazz.getName() , e);
+            log.error("Deserialization Error: {} | Raw response: {}", clazz.getName(), response.getBody(), e);
+            throw new RuntimeException("Deserialization Error: " + clazz.getName(), e);
         }
     }
 
